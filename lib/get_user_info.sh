3c462a99651658918f7062cd58eb0e2f3c4b2e8c
Jason Doyle <jason.doyle@nike.com>
2015-08-22 11:30:38 -0700

    more house cleaning

--- lib/get_user_info.sh
+++ lib/get_user_info.sh
@@ -7,10 +7,12 @@
 get_user_info() {
   curr_user="$(whoami)"
 
-
+  if ( "${curr_user}" != "root" ); then 
     echo ""
     echo "${YELLOW}Please enter an administrative password to get started.${RESET}"
-
+  else
+    echo "I am: $(whoami)"
+  fi
 
   sudo -v
   while true; do sudo -n true; sleep 240; kill -0 "$$" || exit; done 2>/dev/null &
