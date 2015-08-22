8fbbc627ae4ca8e53895a949de23e4bc853fd6cd
Jason Doyle <jason.doyle@nike.com>
2015-08-21 18:53:41 -0700

    updating config files

--- config/lib_loader.sh
+++ config/lib_loader.sh
@@ -5,9 +5,11 @@
 # Manage the sourcing of lib files so they don't muck up the main page
 # ======================================================================
 
+source "${DIR}"/lib/delim_to_array.sh
 source "${DIR}"/lib/inet_check.sh
 source "${DIR}"/lib/results.sh
 source "${DIR}"/lib/dependency_check.sh
-source "${DIR}"/lib/install.sh
-source "${DIR}"/lib/cleanup_install_files.sh
 source "${DIR}"/lib/persist_sudo.sh
+source "${DIR}"/lib/install.sh
+source lib/source_recipes.sh
+source "${DIR}"/lib/cleanup_install_files.sh
